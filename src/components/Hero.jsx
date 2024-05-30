892ec007c67fd9d29e9083da152dd68586fc4b6c
rekamakadi <r.makadi@gmail.com>
2024-05-31 00:09:27 +0200

    [feat] make work download button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import cv from "../assets/reka-makadi-cv.pdf";
 import profilepic from "../assets/profilepic.png";
 import { TypeAnimation } from "react-type-animation";
 import ShinyEffect from "./ShinyEffect";
 
 const Hero = () => {
-  const handleDownloadCV = () => {
-    const link = document.createElement("a");
-    link.setAttribute("download", cv);
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-  };
-
   return (
     <div className="grid md:grid-cols-2 place-items-center max-w-[1250px] mx-auto mb-8 md:mb-32 mt-14">
       <div className="max-w-[800px]">
@@ -29,12 +20,13 @@ const Hero = () => {
 
         <div className="flex flex-row gap-4 mb-4 md:mb-0">
           <button
-            onClick={handleDownloadCV}
             className="transform transition-transform hover:scale-105 hover:shadow-lg
                 z-10 cursor-pointer font-bold text-gray-200 w-1/2 mt-6 p-2
                 bg-gradient-to-r from-[#9333ea] via-[#DE7AFF] to-[#61BAAD] rounded-xl"
           >
-            Download CV
+            <a href="/reka-makadi-cv.pdf" id="download" download="Réka_Makádi_CV.pdf">
+              Download CV
+            </a>
           </button>
         </div>
       </div>
