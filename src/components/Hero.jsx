46cba373cf53f34003852517b68d2e4e27bc93fd
rekamakadi <r.makadi@gmail.com>
2024-06-17 02:06:31 +0200

    [fix] optimize responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ import ShinyEffect from "./ShinyEffect";
 
 const Hero = () => {
   return (
-    <div className="grid md:grid-cols-2 place-items-center max-w-[1300px] mx-auto mb-8 md:mb-32 mt-14">
-      <div className="max-w-[800px]">
-        <p className="text-gray-200 md:text-6xl text-3xl min-w-[600px] tracking-tight">
+    <div className="relative grid md:grid-cols-2 place-items-center max-w-[1300px] mx-auto mb-8 md:mb-32 mt-14 px-4">
+      <div className="max-w-[800px] w-full">
+        <p className="text-gray-200 md:text-6xl text-3xl tracking-tight">
           HEY, I Am <br />
           <span className="">RÉKA MAKÁDI</span> <br />
           <TypeAnimation
@@ -23,13 +23,11 @@ const Hero = () => {
             href="/portfolio/Réka_Makádi_CV.pdf"
             id="download"
             download="Réka_Makádi_CV.pdf"
-            className="flex items-center justify-center transform transition-transform hover:scale-105 hover:shadow-lg
-    z-10 cursor-pointer font-bold text-2xl text-gray-200 w-1/2 mt-6 p-2
-    bg-gradient-to-r from-[#9333ea] via-[#DE7AFF] to-[#61BAAD] rounded-xl"
+            className="flex items-center justify-center transform transition-transform hover:scale-105 hover:shadow-lg z-10 cursor-pointer font-bold text-2xl text-gray-200 w-full md:w-1/2 mt-6 p-2 bg-gradient-to-r from-[#9333ea] via-[#DE7AFF] to-[#61BAAD] rounded-xl"
           >
             Download CV
           </a>
-          <div className="flex w-1/2 mt-6 space-x-4">
+          <div className="flex w-full md:w-1/2 mt-6 space-x-4">
             <a
               href="https://www.linkedin.com/in/rekamakadi"
               target="_blank"
@@ -42,13 +40,20 @@ const Hero = () => {
               href="https://www.github.com/rekamakadi"
               target="_blank"
               rel="noopener noreferrer"
-              className="transform transition-transform hover:scale-105 hover:shadow-lg z-10
-      flex items-center justify-center w-full p-2 bg-gradient-to-r from-[#333333] to-[#6e5494] rounded-xl"
+              className="transform transition-transform hover:scale-105 hover:shadow-lg z-10 flex items-center justify-center w-full p-2 bg-gradient-to-r from-[#333333] to-[#6e5494] rounded-xl"
             >
               <AiFillGithub className="text-gray-200" size={30} />
             </a>
           </div>
         </div>
+      </div>
+
+      <div className="relative w-full flex justify-center md:justify-end mt-8 md:mt-0">
+        <img
+          src="/portfolio/profilepic.png"
+          className="rounded-full w-40 h-40 md:w-60 md:h-60 lg:w-80 lg:h-80 mx-auto md:mx-0"
+          alt="profilepicture"
+        />
       </div>
 
       <div className="absolute inset-0 overflow-hidden md:overflow-visible">
@@ -57,12 +62,6 @@ const Hero = () => {
         </div>
         <ShinyEffect left={-100} top={0} size={1200} />
       </div>
-
-      <img
-        src="/portfolio/profilepic.png"
-        className="rounded-full w-60 h-60 md:w-80 md:h-80 lg:w-100 lg:h-100 mx-auto"
-        alt="profilepicture"
-      />
     </div>
   );
 };
